e198ce583645d41eace29db731db26be19ca64bc
hakunhan <hakunhan@gmail.com>
2021-02-23 20:56:01 +0700

    Update tut04

--- Tut04/app/src/main/java/com/example/tut04/Player.java
+++ Tut04/app/src/main/java/com/example/tut04/Player.java
@@ -1,13 +1,19 @@
 package com.example.tut04;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class Player extends AppCompatActivity {
+    private VideoView videoView;
+    private int currentVideoPos;
+    private String playBack = "PLAYBACK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +23,47 @@ public class Player extends AppCompatActivity {
         Intent intent = getIntent();
         String link = intent.getStringExtra("link");
 
-        VideoView videoView = findViewById(R.id.videoView);
+        videoView = findViewById(R.id.videoView);
         videoView.setVideoPath(link);
         videoView.setMediaController(new MediaController(this));
 
         videoView.start();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        videoView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        currentVideoPos = videoView.getCurrentPosition();
+
+        Log.d("CurrentPos", videoView.getCurrentPosition() + "");
+        Log.d("varCurrentPos", currentVideoPos + "");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        videoView.stopPlayback();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.d("beforeSaveInsCurrentPos", this.currentVideoPos +"");
+        outState.putInt(playBack, this.currentVideoPos);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.d("afterSaveInsCurrentPos", savedInstanceState.getInt("PLAYBACK")+"");
+        videoView.seekTo(savedInstanceState.getInt(playBack));
     }
 }
